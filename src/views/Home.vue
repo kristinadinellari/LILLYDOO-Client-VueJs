65e365957a3f182d6ef59b95a5c6221800ccf5e2
Kristina Dinellari <kristinadinellari@gmail.com>
2019-11-30 16:54:26 +0100

    --add: images ,svg and fonts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <!--<img alt="Vue logo" src="../assets/logo.png">-->
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <h2>Wie es funktioniert</h2>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    // HelloWorld
+  export default {
+    name: 'home',
   }
-}
 </script>
+<style lang="scss">
+  @import '../assets/scss/app.scss';
+
+  h2 {
+    font-family: $font-family;
+    @include fontSizes(20);
+  }
+</style>
